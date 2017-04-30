edc21b29b649c4bcf1c7b45961fd57f4d070fb35
HesterG <hestergong@gmail.com>
2017-04-30 10:37:22 -0400

    09 Scaling App

---
+++ 09 Scaling App/support.js
@@ -0,0 +1,3 @@
+// child_process.exec(command[, options], callback)
+// https://www.tutorialspoint.com/nodejs/nodejs_scaling_application.htm
+console.log("Child Process " + process.argv[2] + " executed." ); 
